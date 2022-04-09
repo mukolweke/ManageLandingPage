8900fb66954eb1aecc9b380087083ffc84d6372e
Michael Mukolwe <michaelolukaka@gmail.com>
2022-04-09 20:34:02 +0300

    Project initialization

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+  <h1 class="text-3xl text-primary-red font-bold underline">Hello world!</h1>
+  <ion-icon name="heart"></ion-icon>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-red': 'hsl(12, 88%, 59%)',
+        'primary-blue': 'hsl(228, 39%, 23%)',
+        'gray-blue': 'hsl(227, 12%, 61%)',
+        'dark-blue': 'hsl(233, 12%, 13%)',
+        'pale-blue': 'hsl(13, 100%, 96%)',
+        'light-gray': 'hsl(0, 0%, 98%)',
+      },
+      fontFamily: {
+        sans: ['Be Vietnam Pro', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
